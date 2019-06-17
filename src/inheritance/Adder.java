b9bab567dd3552997eb72ce1cc2e8259b56d17d6
intern <bawya1098@gmail.com>
2019-06-17 11:57:55 +0530

    solved Easy Inheritance II

---
+++ src/inheritance/Adder.java
@@ -0,0 +1,4 @@
+package inheritance;
+
+class Adder extends Arithmetic {
+}
---
+++ src/inheritance/Arithmetic.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+class Arithmetic {
+    int num1;
+    int num2;
+
+    Arithmetic() {
+        add(num1, num2);
+    }
+
+    int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
